f2a0bdfe22041cffdb4849ae26fcd2f7c540ba4c
Sebastain Velez <svelezg4@eafit.edu.co>
2022-03-19 16:55:44 -0500

    arreglo fallo

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'AdminLTE 3',
+    'title' => 'Admin Panel',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -30,7 +30,7 @@ return [
     |
     */
 
-    'use_ico_only' => false,
+    'use_ico_only' => true,
     'use_full_favicon' => false,
 
     /*
@@ -45,12 +45,12 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo' => '<b>Game</b>Site',
+    'logo_img' => 'img/logo1.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'AdminLTE',
+    'logo_img_alt' => 'Game Site',
 
     /*
     |--------------------------------------------------------------------------
@@ -65,8 +65,8 @@ return [
     */
 
     'usermenu_enabled' => true,
-    'usermenu_header' => false,
-    'usermenu_header_class' => 'bg-primary',
+    'usermenu_header' => true,
+    'usermenu_header_class' => 'bg-secondary',
     'usermenu_image' => false,
     'usermenu_desc' => false,
     'usermenu_profile_url' => false,
@@ -85,10 +85,10 @@ return [
 
     'layout_topnav' => null,
     'layout_boxed' => null,
-    'layout_fixed_sidebar' => null,
-    'layout_fixed_navbar' => null,
-    'layout_fixed_footer' => null,
-    'layout_dark_mode' => null,
+    'layout_fixed_sidebar' => true,
+    'layout_fixed_navbar' => true,
+    'layout_fixed_footer' => true,
+    'layout_dark_mode' => true,
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,13 +47,15 @@
           <a class="btn btn-outline-light mx-3" href="{{ route('login') }}">Login</a>
           <a class="btn btn-outline-light mx-3" href="{{ route('register') }}">Register</a>
         @else
-          <li class="nav-item d-flex align-items-center">
-            <a class="btn btn-outline-light mx-3" href="{{ route('home.about') }}">Account</a>
+        <div class="d-flex flex-row-reverse bd-highlight">
+          <li class="nav-item">
+              <a class="btn btn-outline-light mx-3" href="{{ route('home.about') }}">Account</a>
           </li>
-          <form id="logout" class="d-flex align-items-center" action="{{ route('logout') }}" method="POST">
-          <a role="button" class="btn btn-outline-light mx-3"
+          <form id="logout" action="{{ route('logout') }}" method="POST">
+          <a role="button" class="btn btn-outline-light mx-3 ml-15"
           onclick="document.getElementById('logout').submit();">Logout</a>
-            @csrf
+        </div>
+        @csrf
           </form>
         @endguest
     </div>
