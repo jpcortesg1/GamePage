914e184f9f939245980f5e8280822c5b9cb0daeb
Sebastain Velez <svelezg4@eafit.edu.co>
2022-03-19 18:12:19 -0500

    cambios

--- app/Models/category.php
+++ app/Models/category.php
@@ -5,17 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class game extends Model
+class category extends Model
 {
     use HasFactory;
-    
+    public $timestamps =false;
     protected $fillable = [
         'name',
-        'developer',
-        'description',
-        'releasedate',
-        'price',
-        'agerating',
-        'buyquantity',
+        'description'
     ];
 }
